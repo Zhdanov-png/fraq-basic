62fad8f1c5d9bbe3cd216c86a6b4bd5725449e0a
walerij <becwal@yandex.ru>
2019-11-23 13:29:15 +0300

    Дизайн главной страницы

---
+++ views/site/getstart.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="col-lg-4 col-md-4">
+        <a href="#" class="thumbnail  homesquare">
+            <i class="glyphicon  glyphicon-picture"></i>
+        </a>
+        
+    </div>
+    <div class="col-lg-4 col-md-4 ">
+        <a href="#" class="thumbnail  homesquare">
+            <i class="glyphicon  glyphicon-music"></i>
+        </a>
+        
+    </div>
+    <div class="col-lg-4 col-md-4 ">
+        <a href="#" class="thumbnail  homesquare">
+            <i class="glyphicon  glyphicon-film"></i>
+        </a>
+    </div>
+</div>
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
